fe837be17f275896128d0a55e24d93e59c3c9658
Frane Ćalušić <90317417+FraneCal@users.noreply.github.com>
2023-05-11 14:55:58 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,3 +1,16 @@
 from morse_code import MORSE_CODE_DICT
 
+def to_morse(string):
+  """Function takes a string as input and returns a space-separated string of the Morse code equivalent of the input string. Space between the words is separated by '/' symbol."""
+  alphabet_solution = []
+  for letter in string:
+    #The space between words is separated by the '/' symbol
+    if letter == ' ':
+      alphabet_solution.append('/')
+    else:
+      morse_code = MORSE_CODE_DICT.get(letter)
+      alphabet_solution.append(morse_code)
+  return ' '.join(alphabet_solution)
 
+
+user_input = input('Enter a word you want to convert to Morse code: ').upper()
